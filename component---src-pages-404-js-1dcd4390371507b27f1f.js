5c6ca12617f3f97ecd5dd8f51190ef0e0b9d24d4
Canhua Li <licanhua@live.com>
2021-12-01 00:35:23 -0800

    Updates

---
+++ component---src-pages-404-js-1dcd4390371507b27f1f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklicanhua_blog=self.webpackChunklicanhua_blog||[]).push([[883],{9616:function(t,e,a){a.r(e);var n=a(7294),l=a(7198),u=a(3751);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:o},n.createElement(u.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-1dcd4390371507b27f1f.js.map
